080ae9b72f829460dc020bbba79daf2b677a1da8
Kennedy <kiplangatkennedy019@gmail.com>
2023-03-20 20:37:38 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+//declare CustomerName to bob
+var customerName ="bob"
+var bestCustomer;
+const LeastFavoriteCustomer ="maybe not bob"
+console.log(customerName)
+ function upperCaseCustomerName(){
+     customerName= customerName.toLocaleUpperCase();//changing from lowercase to UPPERCASE
+     return customerName;
+ }
+ function setBestCustomer(){
+    bestCustomer="not bob"
+    return bestCustomer;
+ }
+ function overwriteBestCustomer(){
+     bestCustomer ="maybe bob"
+     return bestCustomer;
+ }
+function changeLeastFavoriteCustomer(){
+    LeastFavoriteCustomer="its bob now";
+}
